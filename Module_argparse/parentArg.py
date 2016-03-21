0d3ebf57026c5423bc0d0f974ebb7680070a229a
tusm <tusm@tusmdeMacBook-Pro-4.local>
2016-03-21 23:41:22 +0800

    add parentarg.py

---
+++ Module_argparse/parentArg.py
@@ -0,0 +1,12 @@
+import argparse
+parent_parser = argparse.ArgumentParser(add_help=False)
+# most parent parsers will specify add_help = False
+parent_parser.add_argument('--parent', type=int)
+
+foo_parser = argparse.ArgumentParser(parents=[parent_parser])
+foo_parser.add_argument('foo')
+foo_parser.parse_args(['--parent', '2', 'xxx'])
+
+bar_parser = argparse.ArgumentParser(parents=[parent_parser])
+bar_parser.add_argument('--bar')
+bar_parser.parse_args(['--parent', 'yyy'])
